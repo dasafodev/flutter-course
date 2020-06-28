211e44e74b899e385c5cc4c01a18c4476a85ba8a
Santiago Forero <ds.foreroa@uniandes.edu.co>
2020-06-28 12:34:09 -0500

    basic style page of disenios is ready with safearea

--- disenios/lib/src/pages/basic_page.dart
+++ disenios/lib/src/pages/basic_page.dart
@@ -1,12 +1,111 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
-  const BasicoPage({Key key}) : super(key: key);
+  final titleStyle = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final subtitleStyle = TextStyle(fontSize: 18.0, color: Colors.grey);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Basico∫'),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+        createImage(),
+        createLabel(),
+        createButton(),
+        createText(),
+        createText(),
+        createText(),
+        createText(),
+      ],
+          ),
+      
+    ));
+  }
+
+  Widget createImage() {
+    return Container(
+      width: double.infinity,
+      child: FadeInImage(
+          placeholder: AssetImage('assets/img/loading.gif'),
+          image: NetworkImage('https://i.vimeocdn.com/video/703876203_640.jpg'),
+          height: 190,
+          fit: BoxFit.cover,
+          ),
+    );
+  }
+
+  Widget createLabel() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Atardecer Bogota', style: titleStyle),
+                  SizedBox(height: 7.0),
+                  Text(
+                    'Bogota, Cundinamarca',
+                    style: subtitleStyle,
+                  ),
+                ],
+              ),
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.red,
+              size: 30.0,
+            ),
+            Text(
+              '41',
+              style: TextStyle(fontSize: 20.0),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget createButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _createAction(Icons.call, 'Call'),
+        _createAction(Icons.near_me, 'Route'),
+        _createAction(Icons.share, 'Share'),
+      ],
+    );
+  }
+
+  Widget _createAction(IconData icon, String text) {
+    return Column(
+      children: <Widget>[
+        Icon(
+          icon,
+          color: Colors.blue,
+          size: 40.0,
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          text,
+          style: TextStyle(color: Colors.blue, fontSize: 14.0),
+        )
+      ],
+    );
+  }
+
+  Widget createText() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+        child: Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', textAlign:TextAlign.justify,),
+      ),
     );
   }
 }
